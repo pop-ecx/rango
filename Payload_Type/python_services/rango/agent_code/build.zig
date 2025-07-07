7cf6829689d0fc5a7d894522f124745db2c77502
pop-ecx <oliver.mulwa@pm.me>
2025-07-07 09:50:59 +0300

    build: default set to releasesmall for linux x86_64

--- Payload_Type/python_services/rango/agent_code/build.zig
+++ Payload_Type/python_services/rango/agent_code/build.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{
+        .default_target = .{
+            .cpu_arch = .x86_64,
+            .os_tag = .linux,
+            .abi = .gnu,
+        },
+    });
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .ReleaseSmall,
+    });
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
