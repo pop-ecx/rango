81b5ab14e512f029bd03986952a0a814dde1c0f6
pop-ecx <oliver.mulwa@protonmail.com>
2026-08-21 12:34:24 +0300

    tests: main file for tests

---
+++ Payload_Type/rango/rango/agent_code/tests/main.zig
@@ -0,0 +1,8 @@
+comptime {
+    _ = @import("execute_cat_test.zig");
+    _ = @import("execute_delete_directory_test.zig");
+    _ = @import("execute_delete_file_test.zig");
+    _ = @import("execute_ls_test.zig");
+    _ = @import("execute_pwd_test.zig");
+    _ = @import("execute_shell_test.zig");
+}
